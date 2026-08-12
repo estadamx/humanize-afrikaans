d24f4da8476a19b9862dba132372c5078926dd24
AJ Esterhuizen <estadamx@users.noreply.github.com>
2026-08-12 16:41:31 +0200

    Add files via upload

--- lib/humanize/locales.rb
+++ lib/humanize/locales.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Humanize
-  %w[az de en es fr fr_ch id ms pt ru th tr jp vi zh_tw].each do |locale|
+  %w[ af az de en es fr fr_ch id ms pt ru th tr jp vi zh_tw].each do |locale|
     autoload locale.split('_').map(&:capitalize).join.to_sym, "humanize/locales/#{locale}.rb"
   end
 end
